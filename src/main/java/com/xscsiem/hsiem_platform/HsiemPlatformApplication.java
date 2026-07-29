c7dba3e791f142848660e33870f455394d035c11
xsc683 <sunxiaochuan56061@gmail.com>
2026-07-29 21:13:15 +0800

    [#ISSUE#1]add frame

---
+++ src/main/java/com/xscsiem/hsiem_platform/HsiemPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.xscsiem.hsiem_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsiemPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsiemPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xscsiem/hsiem_platform/HsiemPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xscsiem.hsiem_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsiemPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
